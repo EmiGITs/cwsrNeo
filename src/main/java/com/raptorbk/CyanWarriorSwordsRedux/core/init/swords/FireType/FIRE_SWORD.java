ef5ff0221905a9504417eb7286971a4c025f75ea
Emiliano <39884304+EmiGITs@users.noreply.github.com>
2024-04-01 03:44:27 -0300

    FIRE_SWORD re implemented

--- src/main/java/com/raptorbk/CyanWarriorSwordsRedux/core/init/swords/FireType/FIRE_SWORD.java
+++ src/main/java/com/raptorbk/CyanWarriorSwordsRedux/core/init/swords/FireType/FIRE_SWORD.java
@@ -1,12 +1,15 @@
 package com.raptorbk.CyanWarriorSwordsRedux.core.init.swords.FireType;
 
+
 import com.raptorbk.CyanWarriorSwordsRedux.config.SwordConfig.SwordConfig;
-import com.raptorbk.CyanWarriorSwordsRedux.core.init.*;
+import com.raptorbk.CyanWarriorSwordsRedux.core.init.ItemInit;
+import com.raptorbk.CyanWarriorSwordsRedux.core.init.SWORD_CWSR;
 import com.raptorbk.CyanWarriorSwordsRedux.core.init.SwordHabilities.ExecuteSeffect;
 import com.raptorbk.CyanWarriorSwordsRedux.core.init.SwordHabilities.SurroundEffect;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.network.chat.Component;
 
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
@@ -16,7 +19,6 @@ import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.TooltipFlag;
-import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
@@ -25,7 +27,9 @@ import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
 
 import net.minecraft.world.item.Tier;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.crafting.Ingredient;
 import net.neoforged.neoforge.common.SimpleTier;
 import net.neoforged.neoforge.common.Tags;
 
@@ -35,9 +39,10 @@ import java.util.List;
 import java.util.Objects;
 
 public class FIRE_SWORD extends SWORD_CWSR {
-
     public static SimpleTier tierIn = new SimpleTier(3, SwordConfig.FIRE_SWORD_DUR.get(), 0.0f, 4.0f, 10, BlockTags.NEEDS_DIAMOND_TOOL, () ->
             Ingredient.of(Tags.Items.ORES_DIAMOND));
+
+
     public FIRE_SWORD( float attackSpeedIn, Properties builder) {
         super(tierIn, SwordConfig.FIRE_SWORD_DMG.get(), attackSpeedIn, builder);
     }
@@ -73,23 +78,47 @@ public class FIRE_SWORD extends SWORD_CWSR {
     }
 
     public InteractionResultHolder<ItemStack> use(Level world, Player entity, InteractionHand handIn) {
+        if(!(world instanceof ServerLevel)) return new InteractionResultHolder<>(InteractionResult.PASS, entity.getItemInHand(handIn));
         ItemStack currentSword = entity.getItemInHand(handIn);
         ItemStack ActiveSynergyTotemStack = new ItemStack(ItemInit.ACTIVE_SYNERGY_TOTEM.get(),1);
 
-        if(!lfAbilityTotem(entity) && ((entity.getMainHandItem() != entity.getItemInHand(handIn) && entity.getMainHandItem().getItem() instanceof SWORD_CWSR && lfActiveSinergyTotem(entity)) || entity.getMainHandItem() == entity.getItemInHand(handIn) || (entity.getOffhandItem()==entity.getItemInHand(handIn) && !(entity.getMainHandItem().getItem() instanceof SWORD_CWSR)))){
+        if(!lfAbilityTotem(entity) && ((entity.getMainHandItem() != entity.getItemInHand(handIn) && entity.getMainHandItem().getItem() instanceof SWORD_CWSR && entity.getInventory().contains(ActiveSynergyTotemStack)) || entity.getMainHandItem() == entity.getItemInHand(handIn) || (entity.getOffhandItem()==entity.getItemInHand(handIn) && !(entity.getMainHandItem().getItem() instanceof SWORD_CWSR)))){
             currentSword.hurtAndBreak(SwordConfig.FIRE_SWORD_USE_COST.get(),entity,Player -> {
+                unlockDestroyACH(entity,world);
                 Player.broadcastBreakEvent(EquipmentSlot.MAINHAND);
             });
         }
 
-        return callerRC(world,entity,handIn,ItemInit.FIRE_SWORD.getId(),SwordConfig.FIRE_SWORD_COOLDOWN.get());
-
+        if(Objects.equals(BuiltInRegistries.ITEM.getKey(entity.getOffhandItem().getItem()),ItemInit.WILD_NATURE.getId())){
+            SWORD_CWSR x = (SWORD_CWSR) entity.getOffhandItem().getItem();
+            x.setDamageBool(true);
+            x.setBlocker(true);
+            InteractionHand f = InteractionHand.OFF_HAND;
+            if(!(entity.getCooldowns().isOnCooldown(x))){
+                ((SWORD_CWSR) x.asItem()).eventRC(world,entity,f,entity.getOffhandItem());
+                entity.getCooldowns().addCooldown(x.asItem(), SwordConfig.WILD_NATURE_SWORD_COOLDOWN.get());
+            }
+            return super.use(world,entity,handIn);
+        }else{
+            if(Objects.equals(BuiltInRegistries.ITEM.getKey(entity.getOffhandItem().getItem()),ItemInit.FIRE_SWORD.getId())){
+                return callerRC(world,entity,handIn,ItemInit.FIRE_SWORD.getId(),SwordConfig.FIRE_SWORD_COOLDOWN.get());
+            }else{
+                if(Objects.equals(BuiltInRegistries.ITEM.getKey(entity.getOffhandItem().getItem()),ItemInit.WATER_SWORD.getId())){
+                    return super.use(world,entity,handIn);
+                }else{
+                    return callerRC(world,entity,handIn,ItemInit.FIRE_SWORD.getId(),SwordConfig.EARTH_SWORD_COOLDOWN.get());
+                }
+            }
+        }
     }
 
     @Override
     public boolean hurtEnemy(ItemStack stack, LivingEntity target, LivingEntity attacker){
         target.setSecondsOnFire(SwordConfig.FIRE_SWORD_HIT_SEC.get());
         stack.hurtAndBreak(SwordConfig.ALL_SWORDS_HIT_COST.get(),attacker,Player -> {
+            if(attacker instanceof Player){
+                unlockDestroyACH((Player) attacker,attacker.getCommandSenderWorld());
+            }
             Player.broadcastBreakEvent(EquipmentSlot.MAINHAND);
         });
         return true;
